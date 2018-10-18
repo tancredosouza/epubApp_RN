5a77c4ea3cef142b0671632a2deb4f9217251be4
tancredosouza <tjssj@cin.ufpe.br>
2018-10-18 17:26:11 -0300

    peu maravilhoso

---
+++ app/src/main/java/com/example/tantan/epubreader/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.tantan.epubreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.folioreader.FolioReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FolioReader folioReader = FolioReader.get();
+        folioReader.openBook(R.raw.moby_dick);
+    }
+}
+

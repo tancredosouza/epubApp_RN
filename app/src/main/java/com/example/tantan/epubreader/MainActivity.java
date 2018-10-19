e1bfeb8cf313def31469029bae1ec96da535452b
tancredosouza <tjssj@cin.ufpe.br>
2018-10-19 11:46:46 -0300

    Removida feature de text-speech. Não precisamos disso

--- app/src/main/java/com/example/tantan/epubreader/MainActivity.java
+++ app/src/main/java/com/example/tantan/epubreader/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.tantan.epubreader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.folioreader.Config;
 import com.folioreader.FolioReader;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Config config = new Config()
+                .setShowTts(false);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FolioReader folioReader = FolioReader.get();
+        FolioReader folioReader = FolioReader.get().setConfig(config, true);
         folioReader.openBook(R.raw.moby_dick);
     }
 }
